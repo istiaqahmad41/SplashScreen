63877d4a8012fcf77d498c4339a528e96a3cc03c
Istiaq Ahmad <istiaqahmad41@gmail.com>
2024-04-23 21:00:53 +0600

    commited

---
+++ app/src/main/java/com/example/mysplash_screen/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.mysplash_screen;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static int splash_time=2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+ new Handler().postDelayed(new Runnable() {
+     @Override
+     public void run() {
+         Intent intent = new Intent(MainActivity.this, splashscreen.class);
+         startActivity(intent);
+         finish();
+     }
+ },splash_time);
+
+
+
+
+    }
+}
